d709ccfaecb945b2104c48dc69963f6225f24c8f
PawankumarPK <pk9910765616@gmail.com>
2020-06-02 23:46:05 +0530

    Setup project with login and signup

---
+++ app/src/main/java/com/handypawan/mvvmexampleproject/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.handypawan.mvvmexampleproject.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.handypawan.mvvmexampleproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
