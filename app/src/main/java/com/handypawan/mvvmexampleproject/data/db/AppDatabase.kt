4b7de8d575035788ad0fdc3bbd6dbfb8e491c7a3
PawankumarPK <pk9910765616@gmail.com>
2020-06-03 11:39:34 +0530

    Setup AppDatabase

--- app/src/main/java/com/handypawan/mvvmexampleproject/data/db/AppDatabase.kt
+++ app/src/main/java/com/handypawan/mvvmexampleproject/data/db/AppDatabase.kt
@@ -1,7 +1,43 @@
 package com.handypawan.mvvmexampleproject.data.db
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.handypawan.mvvmexampleproject.data.db.entities.User
+
 /**
  * Created by pawan on 03,June,2020
  */
-class AppDatabase {
+
+@Database(
+    entities = [User::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getUserDao(): UserDao
+
+    companion object {
+
+        @Volatile
+        private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "MyDatabase"
+            ).build()
+
+
+    }
+
 }
