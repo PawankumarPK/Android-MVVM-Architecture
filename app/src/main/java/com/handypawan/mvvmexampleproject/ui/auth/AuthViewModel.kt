9e32c772a28ca26506fad2d35758a4c71998b70e
PawankumarPK <pk9910765616@gmail.com>
2020-06-08 21:29:08 +0530

    Navigation architecture , navigation drawer , profile fragment

--- app/src/main/java/com/handypawan/mvvmexampleproject/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/handypawan/mvvmexampleproject/ui/auth/AuthViewModel.kt
@@ -84,9 +84,6 @@ class AuthViewModel(private val repository: UserRepository) : ViewModel() {
             return
         }
 
-        /**this is a bad practise because we cannot get instance of another class in a class,
-        So we need to dependency injection*/
-
         Coroutines.main {
             try {
                 val authResponse = repository.userSignup(name!!,email!!, password!!)
